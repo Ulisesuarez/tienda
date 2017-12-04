9cfc9c6d5d183713ab8842d4d3157dfda56f953c
Unknown <ulises203@msn.com>
2017-12-04 21:57:31 +0100

    dando formato a los errorlogs

--- empresa1.py
+++ empresa1.py
@@ -1,19 +1,32 @@
-import utilidades_datos
 import logging
 import os
+import time
+import utilidades_datos
 def accesoCasosTestTxt(matrizCasosTest, rutaAccesoFichero):
-    assert isinstance(matrizCasosTest,list) 
+    try:
+        assert isinstance(matrizCasosTest, list)
+    except AssertionError:
+        logging.info("En día y hora: " + time.strftime("%c"))
+        logging.info(__name__)
+        logging.info("Función "+accesoCasosTestTxt.__name__+" ||  matrizCasosTest = "+str(matrizCasosTest)+" || matrizCasosTest type = "+str(type(matrizCasosTest)))
+        logging.error("assert isinstance(matrizCasosTest,list) \n")
+        
     try:
         if not isinstance(rutaAccesoFichero, str):
-            
             raise ValueError
         fichero = open(rutaAccesoFichero, 'r')
     except FileNotFoundError:
-        logging.error("Fichero no encontrado")
+        logging.info("En día y hora: " + time.strftime("%c"))
+        logging.info(__name__)
+        logging.info("Función "+accesoCasosTestTxt.__name__+" || rutaAccesoFichero = "+rutaAccesoFichero)
+        logging.error(" Fichero no encontrado \n")
         #print("Fichero no encontrado")
         return []
     except ValueError:
-        logging.error("El nombre del fichero ha de ser un string")
+        logging.info("En día y hora: " + time.strftime("%c"))
+        logging.info(__name__)
+        logging.info("Función "+accesoCasosTestTxt.__name__+" || rutaAccesoFichero = "+ str(rutaAccesoFichero) +" || tipo de dato rutaAccesoFichero = "+str(type(rutaAccesoFichero)))
+        logging.error("El nombre del fichero ha de ser un string \n")
         #print("El nombre del fichero ha de ser un string")
         return []
     else:
@@ -30,11 +43,16 @@ def accesoCasosTestTxt(matrizCasosTest, rutaAccesoFichero):
                 item = linea.rstrip().rsplit(',', maxsplit=numeroPropiedadesItem - 1)
                 casosTestDia.append(item)
         fichero.close()
-        
-        utilidades_datos.convertirDeStringADatos(matrizCasosTest)
-        assert len(matrizCasosTest)>0
+        utilidades_datos.convertirStringADatos(matrizCasosTest)
+        try:
+            assert len(matrizCasosTest) > 0
+        except AssertionError:
+            logging.info("En día y hora: " + time.strftime("%c"))
+            logging.info(__name__)
+            logging.info("Función "+accesoCasosTestTxt.__name__+" ||  matrizCasosTest = "+str(matrizCasosTest)+" || matrizCasosTest len = "+len(matrizCasosTest))
+            logging.error("assert len(matrizCasosTest)>0 \n")
         return matrizCasosTest
-"""def accesoCasosTexttest primero comprueba  si el archivo existe y si el nombre es un str , 
+"""def accesoCasosTexttest primero comprueba  si el archivo existe y si el nombre es un str ,
 lee linea a linea el archivo y comprueba primero la linea dia, si es una nueva linea dia crea
 una lista para meter todos los items y su estado en ese dia, comprueba si se ha acabado el día 
 (\n) para agregar la lista del dia a la matriz de casos test que engloba todos los dias,
@@ -44,30 +62,59 @@ Al final crea listas por cada linea de 3 elementos : como si fuera una tabla (Na
 añade estas a otra listea quedando una matriz con los datos de cada día."""
 
 def crearFicheroCasosTest(ficheroVolcadoCasosTest, matrizCasosTest):
-    assert isinstance(matrizCasosTest,list) or isinstance(matrizCasosTest,tuple)
+    try:
+        assert isinstance(matrizCasosTest, list)
+    except AssertionError:
+        logging.info("En día y hora: " + time.strftime("%c"))
+        logging.info(__name__)
+        logging.info("Función "+crearFicheroCasosTest.__name__+" ||  matrizCasosTest = "+str(matrizCasosTest)+" || matrizCasosTest type = "+str(type(matrizCasosTest)))
+        logging.error("assert isinstance(matrizCasosTest, list) \n")
+    try:
+        assert len(matrizCasosTest[0]) > 0
+
+    except (AssertionError,IndexError):
+        logging.info("En día y hora: " + time.strftime("%c"))
+        logging.info(__name__)
+        logging.info("Función "+crearFicheroCasosTest.__name__+" ||  matrizCasosTest = "+str(matrizCasosTest))
+        logging.error("assert len(matrizCasosTest[0])>0 \n")
+
     try:
         if not isinstance(ficheroVolcadoCasosTest, str):
             raise ValueError
         stdout = open(ficheroVolcadoCasosTest, 'w')
     except ValueError:
-            logging.error("La ruta de acceso al fichero ha de ser un string")
+        logging.info("En día y hora: " + time.strftime("%c"))
+        logging.info(__name__)
+        logging.info("Función "+crearFicheroCasosTest.__name__+" || ficheroVolcadoCasosTest = "+ str(ficheroVolcadoCasosTest) +" || tipo de dato ficheroVolcadoCasosTest = "+str(type(ficheroVolcadoCasosTest)))
+        logging.error("La ruta de acceso al fichero ha de ser un string \n")
             #print("La ruta de acceso al fichero ha de ser un string")
     else:
-        utilidades_datos.covertirDeDatosAString(matrizCasosTest)
+        utilidades_datos.convertirDatosAString(matrizCasosTest)
         for (offset, casosTestDia) in enumerate(matrizCasosTest):
             stdout.write('-' * 5 + " Dia %d: " % offset + '-' * 5 + '\n')
             for item in casosTestDia:
                 stdout.write(','.join(item) + '\n')
+        try:
+            assert os.path.exists(ficheroVolcadoCasosTest)
+        except AssertionError:
+            logging.info("En día y hora: " + time.strftime("%c"))
+            logging.info(__name__)
+            logging.info("Función "+accesoCasosTestTxt.__name__+" || ficheroVolacadoCasosTest="+ficheroVolcadoCasosTest)
+            logging.error("assert os.path.exists(ficheroVolcadoCasosTest) \n")
+        try:
+            assert os.stat(ficheroVolcadoCasosTest).st_size > 0
+        except AssertionError:
+            logging.info("En día y hora: " + time.strftime("%c"))
+            logging.info(__name__)
+            logging.info("Función "+accesoCasosTestTxt.__name__ +" || ficheroVolcadoCasosTest = Empty File")
+            logging.error("assert os.stat(ficheroVolcadoCasosTest).st_size > 0 \n")
         
-    assert os.path.exists(ficheroVolcadoCasosTest)
-    assert os.stat(ficheroVolcadoCasosTest).st_size > 0
-    stdout.close()
+        stdout.close()
 
 def mostrarCasosTest(matrizCasosTest):
-    assert isinstance(matrizCasosTest,list)
-    assert matrizCasosTest!=[]
-    assert isinstance(matrizCasosTest[0],list)
-    
+    assert isinstance(matrizCasosTest, list)
+    assert matrizCasosTest != []
+    assert isinstance(matrizCasosTest[0], list)
 
 
     for (offset, casosTestDia) in enumerate(matrizCasosTest):
@@ -77,20 +124,28 @@ def mostrarCasosTest(matrizCasosTest):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='errorlog.log',level=logging.DEBUG)
-    rutaAccesoFichero = "/home/ulises/Micarpeta/proyectos/tienda/casos_tets.txt"
+    logging.basicConfig(filename='Empresa1_error_log.log', level=logging.DEBUG)
+    
+    trutaAccesoFichero = "/home/ulises/Micarpeta/proyectos/tienda/casos_tets.txt"
     # rutaAccesoFichero = "stdout_bug_conjured.gr"
 
-    matrizCasosTest = []
+    tmatrizCasosTest = []
 
-    matrizCasosTest = accesoCasosTestTxt(matrizCasosTest, rutaAccesoFichero)
+    tmatrizCasosTest = accesoCasosTestTxt(tmatrizCasosTest, trutaAccesoFichero)
 
-    mostrarCasosTest(matrizCasosTest)
+    mostrarCasosTest(tmatrizCasosTest)
 
-    ficheroVolcadoCasosTest = "/home/ulises/Micarpeta/proyectos/tienda/stdout.txt"
-
-
-    crearFicheroCasosTest(ficheroVolcadoCasosTest, matrizCasosTest)
+    tficheroVolcadoCasosTest = "/home/ulises/Micarpeta/proyectos/tienda/stdout.txt"
 
 
+    crearFicheroCasosTest(tficheroVolcadoCasosTest, tmatrizCasosTest)
+
+    #errorlog test
+    accesoCasosTestTxt(9,"k")
+    accesoCasosTestTxt([], 87)
+    accesoCasosTestTxt([], "289")
+    crearFicheroCasosTest([], [])
+    #errolog test modulo utilidades
+    utilidades_datos.convertirStringADatos([[["1", "1"]]])
+    utilidades_datos.convertirStringADatos([[["error", "thats not an int"]]])
     
--- utilidades_datos.py
+++ utilidades_datos.py
@@ -1,38 +1,97 @@
 import ast
-def convertirDeStringADatos(matrizCasosTest):
-    assert matrizCasosTest is not None
-    assert isinstance(matrizCasosTest,list)
-    assert matrizCasosTest!=[]
+import logging
+import time
+
+
+def convertirStringADatos(matrizCasosTest):
+    
+    logging.basicConfig(filename='Empresa1_error_log.log', level=logging.DEBUG)
+      
     for matrizDiaIndice in range(len(matrizCasosTest)):
         for listaItemIndice in range(len(matrizCasosTest[matrizDiaIndice])):
             for atributoIndice in range(len(matrizCasosTest[matrizDiaIndice][listaItemIndice])):
                 try:
-                    matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice]= ast.literal_eval(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice].strip())
-                except (SyntaxError,NameError):
+                    matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice] = \
+                    ast.literal_eval(\
+                    matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice].strip())
+                except (SyntaxError, NameError, ValueError):
                     pass
-                if atributoIndice==0:
-                    assert isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice],str)
-                else:
-                    assert isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice],int)
-                            
+                if atributoIndice == 0:
+                    try:
+                        assert isinstance(matrizCasosTest[matrizDiaIndice]\
+                        [listaItemIndice][atributoIndice], str)
+                    except AssertionError:
+                        logging.info("En día y hora: " + time.strftime("%c"))
+                        logging.info(__name__)
+                        logging.info(\
+                            "Función "+convertirStringADatos.__name__+" ||  atributo = "\
+                            +str(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice])\
+                            +" || atributo type = "\
+                            +str(type(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice])))
+                        logging.error("assert isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice],str) \n")
 
+                elif atributoIndice == 1 or atributoIndice == 2:
+                    try:
+                        assert isinstance(matrizCasosTest\
+                        [matrizDiaIndice][listaItemIndice][atributoIndice], int)
+                    except AssertionError:
+                        logging.info("En día y hora: " + time.strftime("%c"))
+                        logging.info(__name__)
+                        logging.info("Función "+convertirStringADatos.__name__\
+                        +" ||  atributo = "+str(matrizCasosTest\
+                        [matrizDiaIndice][listaItemIndice][atributoIndice])\
+                        +" || atributo type = "+str(type(matrizCasosTest\
+                        [matrizDiaIndice][listaItemIndice][atributoIndice])))
+                        logging.error("assert isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice],int) \n")
 
-def covertirDeDatosAString(matrizCasosTest):
-    assert matrizCasosTest is not None
-    assert isinstance(matrizCasosTest,list)
-    assert matrizCasosTest!=[]
+def convertirDatosAString(matrizCasosTest):
+    logging.basicConfig(filename='Empresa1_error_log.log', level=logging.DEBUG)
     for matrizDiaIndice in range(len(matrizCasosTest)):
         for listaItemIndice in range(len(matrizCasosTest[matrizDiaIndice])):
             for atributoIndice in range(len(matrizCasosTest[matrizDiaIndice][listaItemIndice])):
-                
-                if not isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice],str):
+
+                if not isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice], str):
                     try:
-                        matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice]= str(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice])
+                        matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice] = \
+                        str(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice])
                     except SyntaxError:
                         pass
-                assert isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice],str)
+                try:
+                    assert isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice], str)
+                except AssertionError:
+                    logging.info("En día y hora: " + time.strftime("%c"))
+                    logging.info(__name__)
+                    logging.info(\
+                    "Función "+convertirDatosAString.__name__\
+                    +" ||  atributo = "+str(matrizCasosTest\
+                    [matrizDiaIndice][listaItemIndice][atributoIndice])\
+                    +" || atributo type = "+str(type(matrizCasosTest\
+                    [matrizDiaIndice][listaItemIndice][atributoIndice])))
+                    logging.error("assert isinstance(matrizCasosTest[matrizDiaIndice][listaItemIndice][atributoIndice],str) \n")
 
 
+if __name__ == "__main__":# or True :
+    
+    
+
+    #Tests
+    test1antes = [[["test1", "   1", " -2"]]]
+    test1despues = [[["test1", 1, -2]]]
+
+    convertirStringADatos(test1antes)
+    assert test1antes == test1despues
+    test2antes = [[["test2", 1, -2]]]
+    test2despues = [[["test2", "1", "-2"]]]
+    convertirDatosAString(test2antes)
+    assert test2antes == test2despues
+
+    #Errologtest
+    convertirStringADatos([[["1", "1"]]])
+    convertirStringADatos([[["error", "thats not an int"]]])
+
+    
+    
+    
 
 """def deStringADatos(matrizCasosTest):
     for matrizDia in range(len(matrizCasosTest)):
@@ -71,6 +130,4 @@ def covertirDeDatosAString(matrizCasosTest):
                         matrizCasosTest[matrizDia][listaItem][atributo]=False
 
  """
-                
-        
     
