6fa62c3b61c57d4419bfad4eb549438d9e013013
Unknown <ulises203@msn.com>
2017-12-21 17:33:35 +0100

    pasa casos test NormalItem

--- capaLogica/Main.py
+++ capaLogica/Main.py
@@ -14,3 +14,46 @@ print(testItemElixir)
 print(testItemElixir.name)
 print(testItemBrie)
 print(testItemBrie.quality)
+
+class Interfaz:
+
+    def actualizarCalidad(self):
+        pass
+
+class NormalItem(Item,Interfaz):
+
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+    def setquality(self):
+        if self.quality>50:
+            self.quality=50
+        if self.quality<0:
+            self.quality=0
+
+    def setsellin(self):
+        self.sell_in-=1
+
+
+    def actualizarCalidad(self):
+        
+        if self.sell_in<0:
+            self.quality-=2
+            
+        else:
+            self.quality-=1
+        
+        self.setquality()
+
+testItemElixir=NormalItem("Elixir of the Mongoose",5,7)
+
+print(testItemElixir)
+for dia in range(30):
+    
+    testItemElixir.setsellin()
+    testItemElixir.actualizarCalidad()
+    print(testItemElixir)
+
